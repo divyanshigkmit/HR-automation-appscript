d780135a0f53435f500473a88c31863f6c5996d5
divyanshigkmit <divyanshi@gkmit.co>
2023-02-08 10:25:33 +0530

    Feat: Set database connectivity

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const { Client } = require("pg");
+require("dotenv").config();
+
+const client = new Client({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+});
+
+client.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+module.exports = client;
